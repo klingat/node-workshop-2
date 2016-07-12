adc4fcfb8907a5e5a32d062f8e1cc0d921aaf5da
Katarina <klingat@me.com>
2016-07-12 23:36:26 +0000

    added the constructor function to github

--- library/synonyms.js
+++ library/synonyms.js
@@ -36,34 +36,6 @@ SynonymAPI.prototype = {
     }
 };
 
-prompt.get("word", function(err, response) {
-    var api = '3f6b205a1fcabae7c7a2178767fa4eec';
-    
-    if (err) {
-        console.log("There was an error.");
-    }
-    else {
-        var userWord = response.word;
-        var newShit = new SynonymAPI(api);
-
-        newShit.getSynonyms(userWord, function(err, res) {
-            if (err) {
-                console.log("There was an error here.");
-            }
-            else {
-                console.log(res);
-            }
-        });
-    }
-});
-
-
-
-
-
-
-
-
 
 module.exports = {
     SynonymAPI: SynonymAPI
